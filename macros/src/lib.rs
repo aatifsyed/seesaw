35d5aa425becce6785797920f92a7793847d4441
Aatif Syed <aatifsyedyp@gmail.com>
2024-12-07 00:57:22 +0000

    feat: seesaw-macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use proc_macro2::{Span, TokenStream};
+use quote::ToTokens as _;
+use syn::{
+    parse::Nothing, parse_macro_input, token, AttrStyle, Attribute, ImplItem, ItemImpl, Meta, Path,
+    Token,
+};
+
+/// Accept an `impl` block, and add a `#[no_mangle]` attribute to each of the member functions.
+#[proc_macro_attribute]
+pub fn no_mangle(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let config = parse_macro_input!(attr as Nothing);
+    let item = parse_macro_input!(item as ItemImpl);
+    expand(config, item)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn expand(_: Nothing, mut item: ItemImpl) -> syn::Result<TokenStream> {
+    let span = Span::call_site();
+    for item in &mut item.items {
+        if let ImplItem::Fn(it) = item {
+            if !it.attrs.iter().any(|it| it.path().is_ident("no_mangle")) {
+                it.attrs.push(Attribute {
+                    pound_token: Token![#](span),
+                    style: AttrStyle::Outer,
+                    bracket_token: token::Bracket(span),
+                    meta: Meta::Path(Path::from(syn::Ident::new("no_mangle", span))),
+                });
+            }
+        }
     }
+    Ok(item.into_token_stream())
 }
